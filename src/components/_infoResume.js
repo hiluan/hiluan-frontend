6f149408085d78375db29500c52ec8c00a5e5aa1
hiluan <apt.muenchen@gmail.com>
2022-03-16 03:03:50 -0700

    8. Page Resume & fade effect

---
+++ src/components/_infoResume.js
@@ -0,0 +1,45 @@
+export const workExperience = [
+  {
+    position: "Chief Operating Officer",
+    company: "Cinciao Vietnam",
+    companyDescription:
+      "Cinciao is a platform as a service startup. Cinciao.vn allows Vietnamese celebrities to send personalized video messages to their fans in Vietnam.",
+    time: "07/2020 - Present",
+    location: "San Jose, USA",
+    tasks: [
+      "Designing and developing an MVP with Shopify for the startup.",
+      "Designing and implementing business strategies, plans andprocedures.",
+      "Coordinating and building relationships with Vietnamese celebrities and their fans.",
+    ],
+    contactPerson: "",
+  },
+  {
+    position: "Chief Operating Officer",
+    company: "VMilano Fashion",
+    companyDescription:
+      "VMilano is a B2B fashion company in Munich. VMilano gives the local shops a chance to provide their customers the best Italian styles to date.",
+    time: "12/2014 - 06/2020",
+    location: "Munich, Germany",
+    tasks: [
+      "Designing and implementing business operations on all sale channels: B2B shops in Munich, Berlin, Leipzig, Vienna; B2B online shop on vmilano.com; B2C online shops on iounica.com, Amazon.de, Amazon.it, Amazon.fr, Amazon.es, Amazon.co.uk, Amazon.com.",
+      "Establishing policies that promote company culture and vision.",
+      "Overseeing operations of the company and the work of executives.",
+    ],
+    contactPerson:
+      "Contact Person: Luca Lu - VMilano & iounica Founder, CEO | luca@vmilano.com | +49 1765678945",
+  },
+  {
+    position: "Design Architect",
+    company: "Various Architecture Offices",
+    companyDescription:
+      "Worked as a design architect for various architecture firms. Architectural projects ranged from residential designs to commercial designs.",
+    time: "07/2005 - 11/2014",
+    location: "Vietnam - Germany",
+    tasks: [
+      "Meeting with clients to discuss the design requirements for new constructions, alterations, or repairs.",
+      "Providing clients with suitable design recommendations based on factors such as budget, the building site, and recent trends.",
+      "Using CAD software and manual drawing techniques to produce architectural plans for management and client approval.",
+    ],
+    contactPerson: "",
+  },
+];
--- src/pages/PageResume.js
+++ src/pages/PageResume.js
@@ -1,56 +1,131 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { workExperience } from "../components/_infoResume";
 import { isSlidesPlayingAction } from "../store/isSlidesPlayingAction";
 
+const WorkExp = (props) => (
+  <section className="exp-info">
+    <section className="exp-title">
+      <h3 className="h3-bold">{props.exp.position}</h3>
+      <h3>{props.exp.company}</h3>
+    </section>
+    <p className="exp-description">{props.exp.companyDescription}</p>
+    <p className="exp-timeLocation">
+      {props.exp.time} | {props.exp.location}
+    </p>
+    <ul className="exp-tasks">
+      {props.exp.tasks.map((task) => (
+        <li className="exp-task">{task}</li>
+      ))}
+    </ul>
+    <p className="exp-contactPerson">{props.exp.contactPerson}</p>
+  </section>
+);
+const techSkills = [
+  "MongoDB",
+  "Express.JS",
+  "React.JS",
+  "Node.JS",
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "Sass",
+  "Git",
+  "API’s",
+  "Shopify",
+  "Adobe Photoshop",
+  "Adobe XD",
+];
+
 const Resume = () => {
-  //   const [isPlaying, setIsPlaying] = useState(false);
-  //   let nIntervId;
-  //   function startInt() {
-  //     // check if already an interval has been set up
-  //     if (!nIntervId) {
-  //       nIntervId = setInterval(() => console.log("iii"), 500);
-  //     }
-  //   }
-
-  //   function stopInt() {
-  //     clearInterval(nIntervId);
-  //     // release our intervalID from the variable
-  //     nIntervId = null;
-  //   }
-
-  //   const handlerSlidesPlaying = () => {
-  //     if (!isPlaying) {
-  //       startInt();
-  //       setIsPlaying(true);
-  //       console.log(isPlaying);
-  //       return;
-  //     } else {
-  //       stopInt();
-  //       setIsPlaying(false);
-  //       console.log(isPlaying);
-  //       return;
-  //     }
-  //   };
-
-  const [seconds, setSeconds] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds((seconds) => seconds + 1);
-    }, 100);
-    return () => clearInterval(interval);
-  }, []);
+  const experienceList = () => {
+    return workExperience.map((exp) => {
+      return <WorkExp exp={exp} key={exp.company} />;
+    });
+  };
   return (
     <section className="page-others page-resume fade">
-      {/* <button type="button" onClick={handlerSlidesPlaying}>
-        click
-      </button>
-      <button type="button" onClick={handlerSlidesPlaying}>
-        stop
-      </button> */}
-      <header className="a-header">
-        {seconds} seconds have elapsed since mounting.
-      </header>
+      <section className="resume-info">
+        <section>
+          <section className="resume-title">
+            <h2>work experience</h2>
+            <div></div>
+          </section>
+          <section className="resume-body">{experienceList()}</section>
+        </section>
+      </section>
+      <section className="resume-info">
+        <section className="resume-section">
+          <section className="resume-title">
+            <h2>education</h2>
+            <div></div>
+          </section>
+          <section className="resume-body">
+            <section className="exp-title">
+              <h3 className="h3-bold">HCMC University of Architecture</h3>
+              <h3>Bachelor of Architecture</h3>
+            </section>
+            <section className="resume-school">
+              <h3 className="h3-bold">
+                Carinthia University of Applied Sciences
+              </h3>
+              <h3>Master of Architecture</h3>
+            </section>
+          </section>
+        </section>
+        <section className="resume-section">
+          <section className="resume-title">
+            <h2>technical skills</h2>
+            <div></div>
+          </section>
+          <section className="resume-body">
+            <h4 className="resume-skills">
+              {techSkills.map((skill) => (
+                <span>{skill}</span>
+              ))}
+            </h4>
+          </section>
+        </section>
+        <section className="resume-section">
+          <section className="resume-title">
+            <h2>organizations</h2>
+            <div></div>
+          </section>
+          <section className="resume-body">
+            <h3>Munich International Friends</h3>
+            <p className="">
+              Organizing meetups, games, indoor and outdoor activities for
+              international expats and friends in Munich, Germany.
+            </p>
+          </section>
+        </section>
+        <section className="resume-section">
+          <section className="resume-title">
+            <h2>languages</h2>
+            <div></div>
+          </section>
+          <section className="resume-body">
+            <ul className="resume-languages">
+              <li>
+                <span>Vietnamese</span>
+                <p className="resume-language">
+                  Native or Bilingual Proficiency
+                </p>
+              </li>
+              <li>
+                <span>English</span>
+                <p className="resume-language">Full Professional Proficiency</p>
+              </li>
+              <li>
+                <span>German</span>
+                <p className="resume-language">
+                  Professional Working Proficiency
+                </p>
+              </li>
+            </ul>
+          </section>
+        </section>
+      </section>
     </section>
   );
 };
