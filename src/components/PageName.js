dc9f315e3476675f821ee1b19f995a93b4b5c767
hiluan <apt.muenchen@gmail.com>
2022-04-05 14:32:26 -0700

    add > to pagename.js

--- src/components/PageName.js
+++ src/components/PageName.js
@@ -3,17 +3,17 @@ import { useSelector } from "react-redux";
 const switchPages = (value) => {
   switch (value) {
     case "home":
-      return <h3 className="page-name">>Home</h3>;
+      return <h3 className="page-name">> Home</h3>;
     case "apps":
-      return <h3 className="page-name">>My Apps</h3>;
+      return <h3 className="page-name">> My Apps</h3>;
     case "designs":
-      return <h3 className="page-name">>My Designs</h3>;
+      return <h3 className="page-name">> My Designs</h3>;
     case "about":
-      return <h3 className="page-name">>About Me</h3>;
+      return <h3 className="page-name">> About Me</h3>;
     case "resume":
-      return <h3 className="page-name">>My Résumé</h3>;
+      return <h3 className="page-name">> My Résumé</h3>;
     case "contact":
-      return <h3 className="page-name">>Contact Me</h3>;
+      return <h3 className="page-name">> Contact Me</h3>;
     default:
       return <h3 className="page-name"></h3>;
   }
